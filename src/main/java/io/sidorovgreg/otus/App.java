3f1b38a3374459ca8a1fc8f02c4337209b4f8eec
Grigory Sidorov <grigorysidorov@gmail.com>
2021-07-17 20:31:14 +0300

    18 lesson

--- src/main/java/io/sidorovgreg/otus/App.java
+++ src/main/java/io/sidorovgreg/otus/App.java
@@ -1,33 +1,29 @@
 package io.sidorovgreg.otus;
 
-import java.lang.reflect.Constructor;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Objects;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Class<?> clazz = Class.forName("io.sidorovgreg.otus.tasks." + args[0]);
-        Constructor<?> ctor = clazz.getConstructor();
-        Task task = (Task) ctor.newInstance();
-        String path = args[1];
-        for (int i = 0; true; i++) {
-            try {
-                List<String> in = Files.readAllLines(Paths.get(String.format("%s/test.%s.in", path, i)));
-                String out = Files.readAllLines(Paths.get(String.format("%s/test.%s.out", path, i))).stream()
-                        .findFirst()
-                        .orElseThrow(() -> new RuntimeException("expected result isn't exist"));
-                String result = task.execute(in);
-                if (Objects.equals(result, out)) {
-                    System.out.printf("[%s] success%n", in);
-                } else {
-                    System.out.printf("[%s] fail: got %s, expected %s %n", in, result, out);
-                }
-            } catch (NoSuchFileException e) {
-                break;
-            }
-        }
+        ArrayList<KruskalAlgorithm.Edge> graph[] = new ArrayList[4];
+        KruskalAlgorithm.Edge edges[] = new KruskalAlgorithm.Edge[5];
+        for (int i = 0; i < 4; i++)
+            graph[i] = new ArrayList<>();
+        graph[0].add(new KruskalAlgorithm.Edge(0, 1, 5));
+        graph[0].add(new KruskalAlgorithm.Edge(0, 2, 8));
+        graph[1].add(new KruskalAlgorithm.Edge(1, 0, 5));
+        graph[1].add(new KruskalAlgorithm.Edge(1, 2, 10));
+        graph[1].add(new KruskalAlgorithm.Edge(1, 3, 15));
+        graph[2].add(new KruskalAlgorithm.Edge(2, 0, 8));
+        graph[2].add(new KruskalAlgorithm.Edge(2, 1, 10));
+        graph[2].add(new KruskalAlgorithm.Edge(2, 3, 20));
+        graph[3].add(new KruskalAlgorithm.Edge(3, 1, 15));
+        graph[3].add(new KruskalAlgorithm.Edge(3, 2, 20));
+        edges[0] = new KruskalAlgorithm.Edge(0, 1, 5);
+        edges[1] = new KruskalAlgorithm.Edge(0, 2, 8);
+        edges[2] = new KruskalAlgorithm.Edge(1, 2, 10);
+        edges[3] = new KruskalAlgorithm.Edge(1, 3, 15);
+        edges[4] = new KruskalAlgorithm.Edge(2, 3, 20);
+
+        KruskalAlgorithm.findPrintMST(graph, edges);
     }
 }
